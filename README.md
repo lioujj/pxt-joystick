a0657c5da95c3037f6664e0acaf11873fc2a13ab
lioujj <34760803+lioujj@users.noreply.github.com>
2018-09-04 14:48:29 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pxt-joystick
+# joystick
+
+這是專為micro:bit提供的搖桿積木
+
+## License
+
+* MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+joystick=github:lioujj/pxt-joystick
+```
---
+++ main.ts
@@ -0,0 +1,40 @@
+//% weight=0 color=#920dc6 icon="\uf047" block="joystick"
+namespace joystick {
+
+    let myXPin = AnalogPin.P0;
+    let myYPin = AnalogPin.P1;
+    let mySWPin = DigitalPin.P2;
+
+    export enum valueType {
+        //% block="X"
+        X,
+        //% block="Y"
+        Y,
+        //% block="SW"
+        SW
+    }
+
+    //% blockId=setJoystick block="set joystick pins : X pin %XPin|Y pin %YPin|SW pin %SWPin|SW pin pullmode %myPullMode" blockExternalInputs=false
+    //% weight=60
+    //% XPin.fieldEditor="gridpicker" XPin.fieldOptions.columns=4
+    //% XPin.fieldOptions.tooltips="false" XPin.fieldOptions.width="300"
+    //% YPin.fieldEditor="gridpicker" YPin.fieldOptions.columns=4
+    //% YPin.fieldOptions.tooltips="false" YPin.fieldOptions.width="300"
+    //% SWPin.fieldEditor="gridpicker" SWPin.fieldOptions.columns=4
+    //% SWPin.fieldOptions.tooltips="false" SWPin.fieldOptions.width="300"
+    export function setJoystick(XPin: AnalogPin, YPin: AnalogPin, SWPin: DigitalPin, myPullMode: PinPullMode = PinPullMode.PullUp): void {
+        myXPin = XPin;
+        myYPin = YPin;
+        mySWPin = SWPin;
+        pins.setPull(mySWPin, myPullMode);
+    }
+    //% blockId=getJoystickValue block="joystick value of %myType"
+    export function getJoystickValue(myType: valueType): number {
+        switch (myType) {
+            case valueType.X: return pins.analogReadPin(myXPin);
+            case valueType.Y: return pins.analogReadPin(myYPin);
+            case valueType.SW: return pins.digitalReadPin(mySWPin);
+            default: return 0;
+        }
+    }
+}    
